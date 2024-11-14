858c64db3ebaa844d3fd904cc107f4ba3c7cd7a7
Bami O. <bamideleogundeji949@gmail.com>
2024-11-14 17:09:38 -0500

    Add DayManager script and refactor TruthSeekers properties; update MainWorld scene with new component

--- Assets/Scripts/TruthSeekers.cs
+++ Assets/Scripts/TruthSeekers.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class TruthSeekers : ScriptableObject
 {
-    string characterName;
-    Image characterSprite;
-    string characterProblem;
-    string characterSolutions;
-    string characterDialogue;
+    string cName;
+    string fact;
+    int age;
+    string occupation;
+    string problem;
+    List<string[]> solutions;
+    string[] dialogue;
+    Dictionary<string, AnimationClip> animations; // key: animation name, value: animation clip
 
+    //getting and setting
+    public string CName { get {return cName;} }
+
+    public string Fact { get {return fact;} }
+    public int Age { get {return age;} }
+    public string Occupation { get {return occupation;} }
+    public string Problem { get {return problem;} }
+    public List<string[]> Solutions { get {return solutions;} }
+    public string[] Dialogue { get {return dialogue;} }
+    public Dictionary<string, AnimationClip> Animations { get {return animations;} }
 }

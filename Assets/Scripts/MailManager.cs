8be0a60a8c5d1a320047b02a1afc27aeddbac458
Bami O. <bamideleogundeji949@gmail.com>
2024-11-16 11:02:47 -0500

    Refactor MailManager to improve message handling; change Start to Awake method, remove questionBox reference, and update button generation logic for unread and read mail.

--- Assets/Scripts/MailManager.cs
+++ Assets/Scripts/MailManager.cs
@@ -8,40 +8,37 @@ public class MailManager : MonoBehaviour
 {
     [SerializeField] GameObject responseContainer;
     [SerializeField] GameObject letterBox;
-    [SerializeField] GameObject questionBox;
     [SerializeField] Transform messageListContainer;
     [SerializeField] TMP_Text charaResponse;
     [SerializeField] Button buttonPrefab;
     public List<string> unreadMail = new List<string>();
     public List<string> readMail = new List<string>();
 
-    void Start(){
-        letterBox.SetActive(false);
+    void Awake(){
         responseContainer.SetActive(false);
+        letterBox.SetActive(false);
+        
     }
 
     public void OpenMessageList(){
         responseContainer.SetActive(true);
-        questionBox.SetActive(true);
         GenerateButtons();
     }
 
     public void GenerateButtons(){
-        foreach (Transform child in responseContainer.transform)
+        foreach (Transform button in messageListContainer)
         {
-            Destroy(child.gameObject);
+            Destroy(button.gameObject);
         }
 
         foreach (string letter in unreadMail)
         {
             Button button = Instantiate(buttonPrefab, messageListContainer);
-            button.GetComponentInChildren<TMP_Text>().text = letter;
         }
 
         foreach (string letter in readMail)
         {
             Button button = Instantiate(buttonPrefab, messageListContainer);
-            button.GetComponentInChildren<TMP_Text>().text = letter;
         }
     }
 
